a50bdf28530b968e318ef4ca851d2039fedad2f1
eunbiggabi <eunbiggabi84@gmail.com>
2020-10-22 17:17:32 +1000

    done

--- add/add.ts
+++ add/add.ts
@@ -1,13 +1,17 @@
 function add(param1: number, param2: number): number {
 
+    const sum = param1 + param2;
+    return sum;
 }
 
-// function add2(param1: number[]): number {
+function add2(...param1: number[]): number {
 
-// }
-// console.log(add(1, 2));
-// console.log(add(3, 2));
+  return param1.reduce((acc, cur) => acc + cur)
 
-// console.log(add2(1,2,3,4,5));
-// console.log(add2(2,3));
+}
+console.log(add(1, 2));
+console.log(add(3, 2));
+
+console.log(add2(1,2,3,4,5));
+console.log(add2(2,3));
 
