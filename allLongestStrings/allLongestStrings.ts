72ccd027c401a4b39336d63a738f572f5928c0d4
eunbiggabi <eunbiggabi84@gmail.com>
2020-10-23 17:22:27 +1000

    done

--- allLongestStrings/allLongestStrings.ts
+++ allLongestStrings/allLongestStrings.ts
@@ -1,5 +1,34 @@
 function allLongestStrings(inputArray: string[]): string[] {
 
+    // My solution 
+    // let longestString = inputArray[0].length;
+    // const longestStrings = [];
+
+    // // for(let elem of inputArray) {
+    // //     if(longestString <= elem.length) {
+    // //         longestStrings.push(elem)
+    // //     }
+    // // }
+
+    //     inputArray.forEach(function(elem) {
+    //         if (longestString <= elem.length) {
+    //             longestStrings.push(elem)
+    //         }
+    //     });
+
+    // return longestStrings;
+    
+    let longestLength = 0;
+    const longestWords = [];
+
+    inputArray.forEach((word: string) => longestLength = longestLength < word.length ? word.length : longestLength);
+    inputArray.forEach((word: string) => {
+        if(longestLength === word.length) {
+            longestWords.push(word)
+        }
+    });
+    
+    return longestWords
 }
 
 console.log(allLongestStrings(["aba", "aa", "ad", "vcd", "aba"]));
