d62a8545d4c64a35058be8e532a181ca2aaeb969
eunbiggabi <eunbiggabi84@gmail.com>
2020-10-31 15:36:57 +1000

    done

--- areEquallyStrong/areEquallyStrong.ts
+++ areEquallyStrong/areEquallyStrong.ts
@@ -1,7 +1,14 @@
-function areEquallyStrong(yourLeft: number, yourRight: number, friendsLeft: number, friendsRight: number): boolean {
+// function areEquallyStrong(yourLeft: number, yourRight: number, friendsLeft: number, friendsRight: number): boolean {
 
+// }
+
+const areEquallyStrong = (yourLeft: number, yourRight: number, friendsLeft: number, friendsRight: number) =>  {
+    const yourArm = yourLeft + yourRight;
+    const friemdsArm = friendsLeft + friendsRight;
+
+    return yourArm === friemdsArm ? true : false;
 }
 
-// console.log(areEquallyStrong(10, 15, 15, 10))
-// console.log(areEquallyStrong(15, 10, 15, 10))
-// console.log(areEquallyStrong(15, 10, 15, 9))
+console.log(areEquallyStrong(10, 15, 15, 10))
+console.log(areEquallyStrong(15, 10, 15, 10))
+console.log(areEquallyStrong(15, 10, 15, 9))
