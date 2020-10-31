bef1e13ef311a743eb9fcb124047d5e2f30b157d
eunbiggabi <eunbiggabi84@gmail.com>
2020-10-31 14:50:43 +1000

    done

--- alternatingSums/alternatingSums.ts
+++ alternatingSums/alternatingSums.ts
@@ -1,5 +1,24 @@
-function alternatingSums(a: number[]): number[] {
+// function alternatingSums(a: number[]): number[] {
+//     const result = [0, 0]
+   
 
+//     for (let i = 0; i < a.length; i++) {
+//         if (i % 2 === 0) {
+//             result[1] += a[i]
+//         } else {
+//             result[0] += a[i]
+//         }
+//     }
+//     return result;
+
+// }
+
+const alternatingSums = (a: number[]) => {
+    const result = [0, 0]
+        
+    a.forEach((num: number, index: number) => index % 2 === 0 ? result[1] += num : result[0] += num)
+    return result;
 }
 
 console.log(alternatingSums([50, 60, 60, 45, 70]))
+
